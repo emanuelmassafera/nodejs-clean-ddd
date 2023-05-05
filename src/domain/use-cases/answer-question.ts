59b3d7cefaf619c45139ee2a4059dc68bed68532
Emanuel Massafera <emanuel301@live.com>
2023-05-05 10:31:49 -0300

    feat: add answer question use case

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,27 @@
+import UniqueEntityID from '@/core/entities/value-objects/unique-entity-id'
+import Answer from '../entities/answer'
+import AnswersRepository from '../repositories/answers-repository'
+
+interface AnswerQuestionUseCaseRequest {
+  questionId: string
+  instructorId: string
+  content: string
+}
+
+export default class AnswerQuestionUseCase {
+  constructor(
+    private answersRepository: AnswersRepository
+  ) { }
+
+  async execute({ questionId, instructorId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      questionId: new UniqueEntityID(questionId),
+      authorId: new UniqueEntityID(instructorId)
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
