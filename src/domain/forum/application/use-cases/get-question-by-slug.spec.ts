edec63c1d9a3799faddccd8e29e1cd73943d7d07
Emanuel Massafera <emanuel301@live.com>
2023-05-05 15:43:38 -0300

    test: improve tests

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,8 +1,7 @@
-import { Question } from '@/domain/forum/enterprise/entities/question'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Slug } from '../../enterprise/entities/value-objects/slug'
-import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -14,11 +13,8 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      title: 'Example question',
-      content: 'Example content',
+    const newQuestion = makeQuestion({
       slug: Slug.create('example-question'),
-      authorId: new UniqueEntityID(),
     })
     await inMemoryQuestionsRepository.create(newQuestion)
 
