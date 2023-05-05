bd88b63b2aa2ffbb529345b02da738c4363e2e1c
Emanuel Massafera <emanuel301@live.com>
2023-05-05 14:54:39 -0300

    feat: add create question use case

---
+++ test/in-memory-questions-repository.ts
@@ -0,0 +1,12 @@
+import QuestionsRepository from '@/domain/forum/application/repositories/questions-repository'
+import Question from '@/domain/forum/enterprise/entities/question'
+
+export default class InMemoryQuestionsRepository
+  implements QuestionsRepository
+{
+  public items: Question[] = []
+
+  async create(question: Question): Promise<void> {
+    this.items.push(question)
+  }
+}
